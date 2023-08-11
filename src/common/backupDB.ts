90e718708164dd776925a7b4e6f7ad7fa579d210
ducduy0209 <ducduy0209.work@gmail.com>
2023-08-11 21:20:16 +0700

    fix: change way to get timestamp, container name, update: parse connection string

--- src/common/backupDB.ts
+++ src/common/backupDB.ts
@@ -1,15 +1,38 @@
 import { exec } from 'child_process';
-import * as moment from 'moment';
 import * as dotenv from 'dotenv';
 import { loggerIns } from './logger';
 dotenv.config();
 
+function parseConnectionString(connectionString: string) {
+  const matches = connectionString.match(/\/\/(.*?):(.*?)@(.*?):\d+\/(.*)/);
+  if (matches) {
+    const [, username, password, , database] = matches;
+
+    return { username, password, database };
+  }
+  return null;
+}
+
+function formatDateTime(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}_${month}_${day}_${hours}_${minutes}_${seconds}`;
+}
+
 function backupDB() {
-  const timestamp = moment().format('YYYY_MM_DD_HH_mm_ss');
+  const { username, password, database } = parseConnectionString(
+    process.env.DATABASE_URL,
+  );
+  const timestamp = formatDateTime(new Date());
   const backupFileName = `${timestamp}.sql`;
 
   exec(
-    `docker exec ${process.env.CONTAINER_NAME} /usr/bin/mysqldump -u ${process.env.DATABASE_USERNAME} --password=${process.env.DATABASE_PASSWORD} ${process.env.DATABASE_NAME} > backups/${backupFileName}`,
+    `docker exec ${process.env.CONTAINER_NAME} /usr/bin/mysqldump -u ${username} --password=${password} ${database} > backups/${backupFileName}`,
     (error, stdout, stderr) => {
       if (error) {
         loggerIns.error(error.message, { label: 'backupDB' });
