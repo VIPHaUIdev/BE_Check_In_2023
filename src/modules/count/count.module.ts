d06b4e41836649d04ecc899e5baafa204ed599d8
ducduy0209 <ducduy0209.work@gmail.com>
2024-09-18 23:59:06 +0700

    perf: add import dependecies

--- src/modules/count/count.module.ts
+++ src/modules/count/count.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CountService } from './count.service';
 import { CountController } from './count.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
-  providers: [CountService, PrismaService],
+  providers: [CountService, PrismaService, AuthService],
   controllers: [CountController],
   exports: [CountService],
 })
