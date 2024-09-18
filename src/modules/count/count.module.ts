abc8ed733daafea289eae083135a625d96dd1c1e
ducduy0209 <ducduy0209.work@gmail.com>
2024-09-19 00:06:06 +0700

    perf: add import dependecies

--- src/modules/count/count.module.ts
+++ src/modules/count/count.module.ts
@@ -3,9 +3,26 @@ import { CountService } from './count.service';
 import { CountController } from './count.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthService } from '../auth/auth.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [CountService, PrismaService, AuthService],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get<number>(
+            'JWT_ACCESS_EXPIRATION_MINUTES',
+          )}m`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    JwtModule,
+  ],
+  providers: [CountService, PrismaService],
   controllers: [CountController],
   exports: [CountService],
 })
